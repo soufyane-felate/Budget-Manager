7a6c1a97d5b6d062208e8a7f5804ee7f4616d0a2
soufyane-felate <soufyanfellat03@gmail.com>
2025-05-13 17:20:16 +0100

    service

---
+++ src/main/java/com/backend/backend/dto/TransactionDTO.java
@@ -0,0 +1,20 @@
+package com.backend.backend.dto;
+
+
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TransactionDTO {
+    private Long id;
+    private Double amount;
+    private String type;
+    private String description;
+    private LocalDate date;
+    private Long categoryId;
+}
