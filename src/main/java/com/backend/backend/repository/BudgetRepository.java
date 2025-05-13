c5e1fbefcbaacedc1386701dde634910cbaf2be9
soufyane-felate <soufyanfellat03@gmail.com>
2025-05-13 12:00:33 +0100

    -

---
+++ src/main/java/com/backend/backend/repository/BudgetRepository.java
@@ -0,0 +1,7 @@
+package com.backend.backend.repository;
+
+import com.backend.backend.model.Budget;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class BudgetRepository extends JpaRepository<Budget,Long> {
+}
