df72b537a6ac943545c4cbd0f31a8184f6d40370
soufyane-felate <soufyanfellat03@gmail.com>
2025-05-14 16:53:24 +0100

    fix controller

--- src/main/java/com/backend/backend/service/TransactionService.java
+++ src/main/java/com/backend/backend/service/TransactionService.java
@@ -1,10 +1,13 @@
 package com.backend.backend.service;
+
 import com.backend.backend.dto.TransactionDTO;
 import com.backend.backend.mapper.TransactionMapper;
 import com.backend.backend.model.Transaction;
+import com.backend.backend.repository.CategoryRepository;
 import com.backend.backend.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -12,7 +15,7 @@ import java.util.List;
 public class TransactionService {
 
     private final TransactionRepository transactionRepository;
-    //private final CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRepository;
     private final TransactionMapper transactionMapper;
 
     public List<TransactionDTO> getAll() {
@@ -24,21 +27,20 @@ public class TransactionService {
 
     public TransactionDTO add(TransactionDTO dto) {
         Transaction transaction = transactionMapper.toEntity(dto);
-      //  transaction.setCategory(categoryRepository.findById(dto.getCategoryId()).orElseThrow());
+        transaction.setCategory(categoryRepository.findById(dto.getCategoryId()).orElseThrow());
         return transactionMapper.toDTO(transactionRepository.save(transaction));
     }
 
     public TransactionDTO update(Long id, TransactionDTO dto) {
-        Transaction transaction = transactionRepository.findById(Math.toIntExact(id)).orElseThrow();
+        Transaction transaction = transactionRepository.findById(id).orElseThrow();
         transaction.setAmount(dto.getAmount());
         transaction.setDescription(dto.getDescription());
         transaction.setDate(dto.getDate());
-        transaction.setType(dto.getType());
-      //  transaction.setCategory(categoryRepository.findById(dto.getCategoryId()).orElseThrow());
+        transaction.setCategory(categoryRepository.findById(dto.getCategoryId()).orElseThrow());
         return transactionMapper.toDTO(transactionRepository.save(transaction));
     }
 
     public void delete(Long id) {
-        transactionRepository.deleteById(Math.toIntExact(id));
+        transactionRepository.deleteById(id);
     }
 }
